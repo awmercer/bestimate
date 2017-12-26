d0485580cf625e89d79c7685e2ad136ed02c2b96
awmercer <andrew.mercer@gmail.com>
2017-12-26 17:59:50 -0500

    Implemented prediction based estimation in bestimate()

--- R/bestimate.R
+++ R/bestimate.R
@@ -18,18 +18,32 @@ bestimate = function(samp,
                      ref,
                      y_var_names,
                      x_var_names,
-                     sp_wts=NULL,
+                     sp_wts,
                      method = c("propensity", "prediction", "dr"),
-                     posterior_draws = 100) {
+                     posterior_draws = 100,
+                     ... # Additional options passed to BART
+                     ) {
   
   # Check to make sure that the X vars exist and are compatible across
   # samp and ref
   # Check to make sure y_vars exist 
   
+  
+  res = list()
+
   # Fit prediction model for each y_variable specified
   if ("prediction" %in% method) {
-    
-
+    res[["pred"]] = y_var_names %>%
+      set_names(.) %>%
+      map(~pbart2(x.train = select(samp, x_var_names), 
+                  y.train = samp[[.]],
+                  x.test = select(ref, x_var_names),
+                  ...
+                  )) %>%
+      map("yhat.test") %>% # Extract the posterior for the ref sample
+      map(pnorm) %>%       # Convert to probabilities
+      map(~posterior_means(t(.), sp_wts)) %>%
+      bind_rows(.id="y_var")
   }
   
   # If propensity or dr:
@@ -44,6 +58,16 @@ bestimate = function(samp,
   # Append the mean propensity weight to the reference sample
   # Fit a DR model for each outcome variable/synthetic pop
   
-  return(10)
-  
+  return(res)
 }
+
+# Utility function to multiply a matrix of 
+# y values by a matrix of weights and return
+# the weighted posterior distribution for y
+posterior_means = function(y, w) {
+  y = as.matrix(y)
+  w = as.matrix(w)
+  as.tibble(t(y) %*% (w /colSums(w)))
+}
+
+
