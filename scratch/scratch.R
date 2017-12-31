a16d73fd1edcbd48f08b2b026b693fc868f1b7d2
awmercer <andrew.mercer@gmail.com>
2017-12-31 01:42:36 -0500

    Updated scratch

--- scratch/scratch.R
+++ scratch/scratch.R
@@ -7,10 +7,10 @@ np = readRDS("scratch/processed/cleaned_np_civic_data.RDS") %>%
 cps = readRDS("scratch/processed/cps_civic_full_edited.RDS") %>%
   sample_n(200)
 
-weights = fpbb_synth_pops(cps$pwsrwgt, L = 1, N = nrow(cps) * 100)
+sp_weights = fpbb_synth_pops(cps$pwsrwgt, L = 1, N = nrow(cps) * 100)
 
 x_vars = c("age", "sex", "racethn", "educcat", "fcregion")
-y_vars = c("y_always_vote_local")
+y_vars = str_subset(names(np), "^y_")
 
 bart_params = list(ndpost = 25, 
                    keeptrainfits = TRUE,
@@ -18,6 +18,25 @@ bart_params = list(ndpost = 25,
                    ntree = 50)
 
 
-x = bestimate(np, cps, y_vars, x_vars, weights, pred_subsample_size = 150, model_params = bart_params)
+x = bestimate(samp = np, 
+              ref = cps, 
+              y_var_names = y_vars,
+              x_var_names = x_vars,
+              sp_wts = sp_weights, 
+              propensity = TRUE, 
+              prediction = TRUE, 
+              double_robust = TRUE, 
+              dr_propensity_transform = odds, 
+              decompose_bias = TRUE, 
+              pred_subsample_size = 100, 
+              posterior_draws = 1000, 
+              model_params = list(keeptrainfits = TRUE, 
+                                  mc.cores = 1, 
+                                  ntree = 50),
+              seed = 12345)
+
+              
+              
+              
 
 
