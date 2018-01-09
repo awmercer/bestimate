8618017e01302b6b11d8d132bc35c1440cbacfc2
awmercer <andrew.mercer@gmail.com>
2018-01-09 20:45:53 +0000

    Updates to scratch code

--- scratch/scratch.R
+++ scratch/scratch.R
@@ -1,10 +1,11 @@
 
 library(tidyverse)
-np = readRDS("scratch/processed/cleaned_np_civic_data.RDS") %>%
+library(stringr)
+np = readRDS("~/fpbb-inference/data/cleaned/cleaned_np_civic_data.RDS") %>%
   filter(sample_id == "A") %>%
   sample_n(100)
 
-cps = readRDS("scratch/processed/cps_civic_full_edited.RDS") %>%
+cps = readRDS("~/fpbb-inference/data/cleaned/cps_civic_full_edited.RDS") %>%
   sample_n(200)
 
 sp_weights = fpbb_synth_pops(cps$pwsrwgt, L = 1, N = nrow(cps) * 100)
@@ -23,13 +24,15 @@ x = bestimate(samp = np,
               y_var_names = y_vars,
               x_var_names = x_vars,
               sp_wts = sp_weights, 
-              propensity = TRUE, 
+              propensity = FALSE, 
               prediction = TRUE, 
-              double_robust = TRUE, 
+              double_robust_reg = FALSE, 
+              double_robust_wt = FALSE,
               dr_propensity_transform = odds, 
-              decompose_bias = TRUE, 
+              decompose_bias = FALSE, 
+              quality_measures = FALSE,
               pred_subsample_size = 100, 
-              posterior_draws = 1000, 
+              posterior_draws = 100, 
               model_params = list(keeptrainfits = TRUE, 
                                   mc.cores = 1, 
                                   ntree = 50),
