14771891eb81906adc58b65628e3b99974b547e5
awmercer <andrew.mercer@gmail.com>
2018-02-25 23:32:24 +0000

    Added bart functions for balanced/bagged estimation

--- R/bart_functions.R
+++ R/bart_functions.R
@@ -148,3 +148,76 @@ dummify = function(df, drop_unused=TRUE, sep="_") {
 
 }
 
+
+
+# Fits a series of bart models with resamples where the majority class is downsampled
+# to the same size as the minority class. 
+balanced_bagged_bart = function(x.train, y.train, num_fits, weights=NULL, replace=TRUE, mc.cores=1, ...) {
+  
+  if (is.null(weights)) {
+    weights = rep(1, nrow(x.train))
+  }
+  
+  # Stratify x variables according to their class
+  y_list = split(y.train, y.train)
+  x_list = split(x.train, y.train)
+  weight_list = split(weights, y.train)
+  
+  # Find the size of the smalest class
+  tgt_size = reduce(y_list, ~min(length(.)))
+  
+  registerDoParallel(cores=mc.cores)
+  
+  sink("/dev/null")
+  bart_fits = foreach(i=icount(num_fits)) %dopar% {
+    class_indices = map(weight_list, ~sample(x = seq_len(length(.)), 
+                                             size = tgt_size,
+                                             replace = replace, 
+                                             prob = .
+    ))  
+    y = map2(y_list, class_indices, ~.x[.y]) %>% unlist()
+    x = map2(x_list, class_indices, ~slice(.x, .y)) %>% bind_rows()
+    fit = pbart2(x.train = x, y.train=y, nkeeptrain=0, mc.cores=1, ...)
+  }
+  sink()
+  
+  structure(bart_fits, 
+            num_fits = num_fits,
+            class = "pbart_list")
+  
+}
+
+predict.pbart_list = function(object, newdata, num_draws=NULL, mc.cores=1, seed=1234) {
+  
+  loop_cores = 1
+  pred_cores = 1
+  
+  # If there are more fits than cores,
+  # run the loop in parallel. If there are
+  # more cores than fits, use the cores for
+  # predict
+  if (length(object) >= mc.cores) {
+    loop_cores = mc.cores
+  } else {
+    pred_cores = mc.cores
+  }
+  
+  doParallel::registerDoParallel(loop_cores)
+  pos_list = foreach(bm=object) %dopar% {
+    pbart_posterior(bm, newdata=newdata, mc.cores=pred_cores)
+  } 
+  
+  pos_preds = pos_list %>% bind_cols() 
+  if (!is.null(num_draws)) {
+    set.seed(seed)
+    pos_preds = pos_preds %>% 
+      select(sample(1:ncol(.), size = num_draws, replace = FALSE))
+  }
+  names(pos_preds) = glue("draw_{seq_along(pos_preds)}")
+  return(pos_preds)
+}
+
+
+
+
+
