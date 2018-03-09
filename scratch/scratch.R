f0e1c10dccb1dfbfb82939dbd3f6d5ad6e282625
awmercer <andrew.mercer@gmail.com>
2018-03-09 02:58:27 +0000

    Updated scrach for testing on Mac

--- scratch/scratch.R
+++ scratch/scratch.R
@@ -4,58 +4,64 @@ library(stringr)
 np = readRDS("~/fpbb-inference/data/cleaned/cleaned_np_civic_data.RDS") %>%
   filter(sample_id == "A")
 
-cps = readRDS("~/fpbb-inference/data/cleaned/cps_civic_full_edited.RDS") %>%
-  sample_n(5000)
+cps = readRDS("~/fpbb-inference/data/cleaned/cps_civic_full_edited.RDS") 
 
 sp_weights = fpbb_synth_pops(cps$pwsrwgt, L = 1, N = nrow(cps) * 100)
 
 x_vars = c("age", "sex", "racethn", "educcat", "fcregion")
 y_vars = str_subset(names(np), "^y_")
 
-bart_params = list(ndpost = 25, 
-                   keeptrainfits = TRUE,
-                   mc.cores = 3, 
-                   ntree = 50)
 
-x_samp = np[, x_vars]
-x_ref = cps[, x_vars]
+b = pbart2(np[, x_vars], np$y_talk_neighbor_weekly)
 
-p = imap(sp_weights, ~estimate_propensities(sp_wt = .x, sp_id = .y, x_samp = x_samp, x_ref = x_ref, num_replicates = 2,
-                                            bart_params = bart_params))
+b = balanced_bagged_bart(x.train = np[, x_vars], y.train = np$y_always_vote_local, num_fits = 1)
 
-ref = cps
-samp = np
-
-comb = bind_rows(ref %>% mutate(smp = 0), 
-                 samp %>% mutate(smp = 1)) %>%
-  select(x_vars, smp)
-
-
-# p = balanced_bagged_bart(x.train = comb[, x_vars], 
-#                          y.train = comb[["smp"]], 
-#                          num_fits = 2, mc.cores = 2)
-# 
-# x = predict(p, newdata = samp)
-
-x = bestimate(samp = np, 
+reps1 = bestimate(samp = np, 
+                   ref = cps, 
+                   y_var_names = y_vars[1],
+                   x_var_names = x_vars,
+                   sp_wts = sp_weights, 
+                   propensity = T, 
+                   prediction = F, 
+                   double_robust_reg = F, 
+                   double_robust_wt = F,
+                   dr_propensity_transform = log, 
+                   propensity_replicates = 1,
+                   quality_measures = F,
+                   posterior_draws = 1000, 
+                   bart_params = list(mc.cores = 10, 
+                                      ntree = 50))
+reps10 = bestimate(samp = np, 
               ref = cps, 
-              y_var_names = y_vars,
+              y_var_names = y_vars[1],
               x_var_names = x_vars,
               sp_wts = sp_weights, 
               propensity = T, 
-              prediction = T, 
-              double_robust_reg = T, 
-              double_robust_wt = T,
+              prediction = F, 
+              double_robust_reg = F, 
+              double_robust_wt = F,
               dr_propensity_transform = log, 
-              quality_measures = T,
-              pred_subsample_size = 100, 
-              posterior_draws = 100, 
-              bart_params = list(keeptrainfits = TRUE, 
-                                  mc.cores = 3, 
+              propensity_replicates = 10,
+              quality_measures = F,
+              posterior_draws = 1000, 
+              bart_params = list(mc.cores = 10, 
                                   ntree = 50))
 
-              
-              
+reps20 = bestimate(samp = np, 
+                   ref = cps, 
+                   y_var_names = y_vars[1],
+                   x_var_names = x_vars,
+                   sp_wts = sp_weights, 
+                   propensity = T, 
+                   prediction = F, 
+                   double_robust_reg = F, 
+                   double_robust_wt = F,
+                   dr_propensity_transform = log, 
+                   propensity_replicates = 20,
+                   quality_measures = F,
+                   posterior_draws = 1000, 
+                   bart_params = list(mc.cores = 10, 
+                                      ntree = 50))              
               
 
-
+var(reps1$y_bar_propwt)
